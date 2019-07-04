27fe5314bac6328168559554dd9f2b8668533c04
Shankar <shankar21mail@gmail.com>
2019-07-04 15:05:59 +0530

    Chore: Refactor `getAppFolderPath` using electron API

--- app/utils/app_utils.ts
+++ app/utils/app_utils.ts
@@ -1,7 +1,4 @@
-import os from 'os';
-import path from 'path';
-
-import pkg from '$Package';
+import { app } from 'electron';
 
 export const generateRandomString = (): string => {
     return (
@@ -14,24 +11,7 @@ export const generateRandomString = (): string => {
     );
 };
 
-export const getAppFolderPath = () => {
-    const platform = os.platform();
-    const appName = pkg.name;
-    let userData;
-    if ( platform === 'win32' ) {
-        userData = path.join( process.env.APPDATA, appName );
-    } else if ( platform === 'darwin' ) {
-        userData = path.join(
-            process.env.HOME,
-            'Library',
-            'Application Support',
-            appName
-        );
-    } else {
-        userData = path.join( 'var', 'local', appName );
-    }
-    return userData;
-};
+export const getAppFolderPath = () => app.getPath( 'appData' );
 
 export const databaseCallBackHandler = ( resolve, reject ) => {
     return ( success, data ) => {
