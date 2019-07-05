d2180cbcf4ecee24c9432f252d0787878b7de5d6
Shankar <shankar21mail@gmail.com>
2019-07-05 13:54:28 +0530

    Fix: Resolve type-check and update jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,45 @@
+// module.exports = {
+//     preset: 'ts-jest',
+//     testEnvironment: 'node',
+//     testURL: 'http://localhost/',
+//     verbose: true,
+//     moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
+//     setupFiles: ['raf/polyfill', '<rootDir>/tests_setup.js'],
+//     testPathIgnorePatterns: ['node_modules'],
+//     moduleDirectories: ['app', 'test', 'node_modules'],
+//     moduleNameMapper: {
+//         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+//             '<rootDir>/mocks/fileMock.ts',
+//         '\\.(css|less|scss)$': '<rootDir>/mocks/fileMock.ts',
+//         '^spectron-lib(.*)$': '<rootDir>/__e2e__/lib$1'
+//     }
+// };
+
+const { pathsToModuleNameMapper } = require( 'ts-jest/utils' );
+const { compilerOptions } = require( './tsconfig' );
+
+const tsConfigAlias = pathsToModuleNameMapper( compilerOptions.paths, {
+    prefix: '<rootDir>/'
+} );
+
 module.exports = {
-    testURL: 'http://localhost/',
     verbose: true,
-    moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
-    setupFiles: ['raf/polyfill', '<rootDir>/tests_setup.js'],
-    testPathIgnorePatterns: ['node_modules'],
-    moduleDirectories: ['app', 'test', 'node_modules'],
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest'
+    },
     moduleNameMapper: {
-        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-            '<rootDir>/mocks/fileMock.ts',
-        '\\.(css|less|scss)$': '<rootDir>/mocks/fileMock.ts',
-        '^spectron-lib(.*)$': '<rootDir>/__e2e__/lib$1'
-    }
+        ...tsConfigAlias,
+        '\\.(css|less|scss|sss|styl)$':
+            '<rootDir>/node_modules/jest-css-modules'
+    },
+    globals: {
+        'ts-jest': {
+            diagnostics: {
+                pathRegex: '\\.(spec|test)\\.ts$'
+            }
+        }
+    },
+    setupFiles: ['<rootDir>/tests_setup.ts']
 };
