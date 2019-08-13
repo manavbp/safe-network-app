fbb52678a969d3e80ac8b2952d8bc25739f7b0d7
Manav Chhabria <manavbp@gmail.com>
2019-08-13 13:03:29 +0530

    Fix: Update to Onboarding spec tests

--- __tests__/components/OnBoarding.spec.tsx
+++ __tests__/components/OnBoarding.spec.tsx
@@ -103,7 +103,6 @@ describe( 'On Boarding', () => {
             .find( Stepper )
             .props()
             .onNext();
-        expect( props.storePreferences ).toHaveBeenCalled();
         expect( props.setAppPreferences ).toHaveBeenCalled();
     } );
 } );
