e0fd0f4cbc6840fb41786b891770863eb264c55e
Josh Wilson <joshuef@gmail.com>
2019-11-13 08:50:45 +0100

    tests: Add vault config install for CI

--- __e2e__/Permissions/Request.e2e.ts
+++ __e2e__/Permissions/Request.e2e.ts
@@ -2,6 +2,7 @@ import { Selector } from 'testcafe';
 import { waitForReact } from 'testcafe-react-selectors';
 import { clickOnMainMenuItem } from 'testcafe-browser-provider-electron';
 import { getPageUrl, getPageTitle, getByAria } from '../helpers';
+import { isCI } from '$Constants';
 
 const assertNoConsoleErrors = async ( t ): Promise<void> => {
     const { error } = await t.getBrowserConsoleMessages();
@@ -24,6 +25,12 @@ fixture`Permission Request Flow`
 // TODO: Setup e2e to always have authd running. (as part of app?)
 // Right now authd must be setup and running manually
 test( 'can navigate to login, and attempt/fail to login', async ( t ) => {
+    if ( isCI ) {
+        // @ts-ignore
+        await clickOnMainMenuItem( ['Help', 'Update shared vault config'] );
+        await t.wait( 5000 );
+    }
+
     const loginButton = getByAria( 'Login Button' );
     const password = getByAria( 'Password Field' );
     const passphrase = getByAria( 'Passphrase Field' );
